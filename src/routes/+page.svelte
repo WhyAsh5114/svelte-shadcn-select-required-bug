bcc20b88aea5a2d7e27e96ffc5e61245875b1a3a
WhyAsh5114 <whyash5114@gmail.com>
2024-03-23 21:31:24 +0530

    created basic demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import * as Select from "$lib/components/ui/select/index.js";
+
+  const fruits = [
+    { value: "apple", label: "Apple" },
+    { value: "banana", label: "Banana" },
+    { value: "blueberry", label: "Blueberry" },
+    { value: "grapes", label: "Grapes" },
+    { value: "pineapple", label: "Pineapple" },
+  ];
+</script>
+
+<form
+  class="flex gap-2"
+  on:submit|preventDefault={() =>
+    alert("shouldn't happen when nothing selected")}
+>
+  <Select.Root required>
+    <Select.Trigger class="w-[180px]">
+      <Select.Value placeholder="Select a fruit" />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Group>
+        <Select.Label>Fruits</Select.Label>
+        {#each fruits as fruit}
+          <Select.Item value={fruit.value} label={fruit.label}>
+            {fruit.label}
+          </Select.Item>
+        {/each}
+      </Select.Group>
+    </Select.Content>
+    <Select.Input name="favoriteFruit" />
+  </Select.Root>
+  <Button type="submit">Submit</Button>
+</form>
